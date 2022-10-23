8bc7ce48d5dd89e572a59ae286ae96c33c80f2e3
taiyipan <taiyipan@gmail.com>
2022-10-23 00:51:59 -0400

    increased thread_queue_size from 8 to 512; average fps increased

--- rgb_camera_stream/stream.sh
+++ rgb_camera_stream/stream.sh
@@ -1,10 +1,12 @@
 ffmpeg -f v4l2 \
        -input_format mjpeg \
        -video_size 640x480 \
+       -thread_queue_size 512 \
        -i /dev/video0 \
        -f v4l2 \
        -input_format mjpeg \
        -video_size 640x480 \
+       -thread_queue_size 512 \
        -i /dev/video2 \
        -c:v copy -map 0 output_0.mkv \
        -c:v copy -map 1 output_2.mkv

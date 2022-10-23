3dc187dd787c16425fb20fab24cf3e01980774a5
taiyipan <taiyipan@gmail.com>
2022-10-23 15:50:04 -0400

    defined variables as hyperparams for tuning

--- rgb_camera_stream/stream.sh
+++ rgb_camera_stream/stream.sh
@@ -1,14 +1,17 @@
+#!/usr/bin/env bash
+cam1_port=0;
+cam2_port=2;
+width=800;
+height=600;
 ffmpeg -f v4l2 \
        -input_format mjpeg \
-       -framerate 30 \
-       -video_size 640x480 \
+       -video_size ${width}x${height} \
        -thread_queue_size 512 \
-       -i /dev/video0 \
+       -i /dev/video${cam1_port} \
        -f v4l2 \
        -input_format mjpeg \
-       -framerate 30 \
-       -video_size 640x480 \
+       -video_size ${width}x${height} \
        -thread_queue_size 512 \
-       -i /dev/video2 \
-       -c:v copy -map 0 output_cam0_$(date +"%Y-%m-%d_%H:%M:%S").mkv \
-       -c:v copy -map 1 output_cam2_$(date +"%Y-%m-%d_%H:%M:%S").mkv
+       -i /dev/video${cam2_port} \
+       -c:v copy -map 0 output_cam${cam1_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv \
+       -c:v copy -map 1 output_cam${cam2_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv

f30d36843897fd05b7a016f1a5b4c825445bc561
taiyipan <taiyipan@gmail.com>
2022-10-24 01:18:22 -0400

    added encoding feature to reduce output file size dramatically; increased CPU and RAM load

--- rgb_camera_stream/stream.sh
+++ rgb_camera_stream/stream.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 cam1_port=0;
 cam2_port=2;
-width=800;
-height=600;
+width=640;
+height=480;
+fps=10;
+queue_size=1024;
 ffmpeg -f v4l2 \
        -input_format mjpeg \
        -video_size ${width}x${height} \
-       -thread_queue_size 512 \
+       -thread_queue_size ${queue_size} \
        -i /dev/video${cam1_port} \
        -f v4l2 \
        -input_format mjpeg \
        -video_size ${width}x${height} \
-       -thread_queue_size 512 \
+       -thread_queue_size ${queue_size} \
        -i /dev/video${cam2_port} \
-       -c:v copy -map 0 output_cam${cam1_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv \
-       -c:v copy -map 1 output_cam${cam2_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv
+       -c:v libx264 -r ${fps} -map 0 output_cam${cam1_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv \
+       -c:v libx264 -r ${fps} -map 1 output_cam${cam2_port}_$(date +"%Y-%m-%d_%H:%M:%S").mkv
